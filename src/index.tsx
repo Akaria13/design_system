3d27947a950163ee2c5d70056fc2432c23a8125b
Akash <72251997+Akaria13@users.noreply.github.com>
2023-04-17 17:39:30 +0530

    Updates

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as Button from "./components/Button/Button";
-import * as IconButtons from "./components/Button/IconButtons";
-import * as InputField from "./components/Input/InputField";
-import * as InputPin from "./components/InputPin/InputPin";
-import * as TextArea from "./components/TextArea/TextArea";
-
-export { Button, IconButtons, InputField, InputPin, TextArea };
-export * from "./Icons/IconList";
